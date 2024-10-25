40009381f62b11c0013edf80e8bca151f18adaca
Joni Eren <oni.rene@outlook.com>
2024-10-25 10:48:43 -0500

    changes

---
+++ src/root/script.js
@@ -0,0 +1,38 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    // =-=-=-=-=-=-=-= [ SIDE NAV BAR ] =-=-=-=-=-=-=-=
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+document.addEventListener('DOMContentLoaded', function() {
+  
+  // =-=-=-=-=-=-=-= [ DARK - MODE  ] =-=-=-=-=-=-=-=
+  // const darkModeButton = document.getElementById('dark-mode');
+  // darkModeButton.addEventListener('click', function() {
+  //   document.body.classList.toggle('dark-mode');
+  // });
+
+
+  
+  // =-=-=-=-=-=-=-= [ MOBILE PHONE - CARDS ] =-=-=-=-=-=-=-=
+  // const cards = document.querySelectorAll('.card');
+  // cards.forEach(card => {
+  //     card.addEventListener('click', function(e) {
+  //         // Prevent redirect if the click is directly on the link or any interactive element
+  //         if (e.target.tagName !== 'A' && e.target.tagName !== 'BUTTON' && window.innerWidth <= 600) {
+  //             const link = card.querySelector('.btn-group a');
+  //             if (link) {
+  //                 window.location.href = link.href;
+  //             }
+  //         }
+  //     });
+  // });
+
+
+});
+
+
+
+// ======================================================
+  // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+      // ~ ~ ~ ~ ~ ~ ~ [ /SCRIPT ] ~ ~ ~ ~ ~ ~ ~
+  // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// ======================================================
